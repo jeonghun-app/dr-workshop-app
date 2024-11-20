060a306f909e2098964c212c99d677d8997b5860
Ubuntu <ubuntu@ip-172-31-7-147.ap-northeast-2.compute.internal>
2024-11-20 15:12:36 +0000

    update

--- web-app/stores/authStore.ts
+++ web-app/stores/authStore.ts
@@ -1,4 +1,3 @@
-// stores/authStore.ts
 import { create } from 'zustand';
 
 interface AuthState {
@@ -7,18 +6,36 @@ interface AuthState {
   token: string | null;
   login: (token: string, userId: string) => void;
   logout: () => void;
+  initializeAuth: () => void; // 초기화 함수 추가
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   isAuthenticated: false,
   userId: null,
   token: null,
+
+  // 로그인 메서드
   login: (token: string, userId: string) => {
     localStorage.setItem('token', token);
+    localStorage.setItem('userId', userId);
     set({ isAuthenticated: true, userId, token });
   },
+
+  // 로그아웃 메서드
   logout: () => {
     localStorage.removeItem('token');
+    localStorage.removeItem('userId');
     set({ isAuthenticated: false, userId: null, token: null });
+  },
+
+  // 초기화 메서드
+  initializeAuth: () => {
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem('token');
+      const userId = localStorage.getItem('userId');
+      if (token && userId) {
+        set({ isAuthenticated: true, token, userId });
+      }
+    }
   },
 }));
